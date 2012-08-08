cb70be2e1fe9ddd47880420820d93204cf46f922
Ludovic Rousseau <ludovic.rousseau@free.fr>
2012-08-08 08:34:36 +0000

    Make the sample code a real and working example
    git-svn-id: svn://svn.debian.org/svn/pcsclite/trunk/contrib@6407 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- libPCSCv2part10/sample.c
+++ libPCSCv2part10/sample.c
@@ -1,3 +1,26 @@
+/*
+    sample.c: example of use of libPCSCv2part10 helper functions
+    Copyright (C) 2012   Ludovic Rousseau
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+ * $Id$
+ */
+
 #include <stdio.h>
 
 #ifdef __APPLE__
@@ -11,15 +34,52 @@
 
 #include "PCSCv2part10.h"
 
+/* PCSC error */
+#define PCSC_ERROR_EXIT(rv) \
+if (rv != SCARD_S_SUCCESS) \
+{ \
+	printf("Failed at line %d with %s (0x%lX)\n", __LINE__, pcsc_stringify_error(rv), rv); \
+	goto end; \
+}
+
 int main(void)
 {
-	SCARDHANDLE hCard = 0;
-	int value, ret;
+	LONG rv;
+	SCARDCONTEXT hContext;
+	SCARDHANDLE hCard;
+	int value, ret = -1;
+	DWORD dwReaders, dwPref;
+	char *mszReaders;
 
-	ret = PCSCv2Part10_find_TLV_property_by_tag_from_hcard(hCard, PCSCv2_PART10_PROPERTY_wIdVendor, &value);
+	rv = SCardEstablishContext(SCARD_SCOPE_SYSTEM, NULL, NULL, &hContext);
+	PCSC_ERROR_EXIT(rv)
 
-	printf("This sample does nothing.\n");
-	printf("It is just used to check compilation and link\n");
+	dwReaders = SCARD_AUTOALLOCATE;
+	rv = SCardListReaders(hContext, NULL, (LPSTR)&mszReaders, &dwReaders);
+	PCSC_ERROR_EXIT(rv)
 
+	/* use first reader */
+	printf("Using reaer: %s\n", mszReaders);
+
+	rv = SCardConnect(hContext, mszReaders,
+		SCARD_SHARE_SHARED, SCARD_PROTOCOL_T0 | SCARD_PROTOCOL_T1,
+		&hCard, &dwPref);
+
+	ret = PCSCv2Part10_find_TLV_property_by_tag_from_hcard(hCard,
+		PCSCv2_PART10_PROPERTY_wIdVendor, &value);
+
+	printf("ret: %d\n", ret);
+	printf("value for PCSCv2_PART10_PROPERTY_wIdVendor: 0x%04X\n", value),
+
+	rv = SCardDisconnect(hCard, SCARD_LEAVE_CARD);
+	PCSC_ERROR_EXIT(rv)
+
+	rv = SCardFreeMemory(hContext, mszReaders);
+	PCSC_ERROR_EXIT(rv)
+
+	rv = SCardReleaseContext(hContext);
+	PCSC_ERROR_EXIT(rv)
+
+end:
 	return ret;
 }
